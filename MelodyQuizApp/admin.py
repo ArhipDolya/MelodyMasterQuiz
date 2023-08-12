1e2957e7a81252feebf7e5c75a264758e6cbf41e
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2023-08-12 15:59:57 +0300

    Create models in MelodyQuizApp for Quiz game functionality and register them in admin.py

--- MelodyQuizApp/admin.py
+++ MelodyQuizApp/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Question, Answer, UserProgress, GameStatistic
 
-# Register your models here.
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('text',)
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('text', 'question', 'is_correct') 
+
+@admin.register(UserProgress)
+class UserProgressAdmin(admin.ModelAdmin):
+    list_display = ('user', 'question', 'answer', 'created_at') 
+
+@admin.register(GameStatistic)
+class GameStatisticAdmin(admin.ModelAdmin):
+    list_display = ('user', 'correct_answers', 'total_questions', 'created_at')
--- MelodyQuizApp/models.py
+++ MelodyQuizApp/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Question(models.Model):
+    text = models.CharField(max_length=500)
+
+
+    def __str__(self):
+        return self.text
+    
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=100)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
+
+
+class UserProgress(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.question.text}'
+    
+class GameStatistic(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    correct_answers = models.PositiveIntegerField(default=0)
+    total_questions = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f'{self.user.username} - {self.correct_answers}/{self.total_questions}' 
