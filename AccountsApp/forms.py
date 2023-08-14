e6c1acec117ab850ed85139b7a055e29bc17b770
ArhipKokocan <hts1345hjyuhip228888@gmail.com>
2023-08-14 20:02:37 +0300

    Add user registration functionality and styling

--- AccountsApp/forms.py
+++ AccountsApp/forms.py
@@ -1,10 +1,14 @@
 from django import forms
 from django.contrib.auth import authenticate
+from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 
 class RegisterForm(UserCreationForm):
-    pass
+    
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
 
 
 
--- AccountsApp/views.py
+++ AccountsApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import LoginForm, RegisterForm
 from django.contrib.auth import login, authenticate
+from django.contrib import messages
 
 
 def login_view(request):
@@ -24,4 +25,20 @@ def login_view(request):
 
 
 def register_view(request):
-    return render(request, 'AccountsApp/registration.html') 
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+
+            # Specify the authentication backend
+            user.backend = 'django.contrib.auth.backends.ModelBackend'
+
+            login(request, user)
+            return redirect('MelodyQuizApp:quiz_game_view')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'AccountsApp/registration.html', {'form': form})
